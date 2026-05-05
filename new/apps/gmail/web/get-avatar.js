d8081ca1dc168135b3857d419b87bc1758ad56ac
tiennt <tokentien@gmail.com>
2026-05-06 06:21:43 +0700

    a

--- new/apps/gmail/web/get-avatar.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var img = document.querySelector('img[aria-label*="Google"], img[data-profile-identifier], a[href*="accounts.google.com"] img');
-  if (img && img.src && img.src.indexOf('googleusercontent.com') !== -1) return img.src;
-  var av = document.querySelector('header img[src*="googleusercontent"]');
-  return av && av.src ? av.src : null;
-})();
